5199ee294eebf709df4ca576cad832b0d9c4e9c1
Ethan C <ethan.cho1826@gmail.com>
2021-10-19 23:05:56 -0500

    post route

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -11,7 +11,22 @@ notes.get('/', (req, res) => {
 
 notes.post('/', (req, res) => {
     console.log(`${req.method} received`);
-    res.send(`${req.method} received`)
+    console.log(req.body);
+    const {title, text} = req.body;
+    if(title && text) {
+        const newNote = {title, text};
+
+        readAppend(newNote, './db/db.json');
+
+        const response = {
+            status: "success",
+            body: newNote
+        };
+
+        res.json(response);
+    }else {
+        res.json("Error in posting note");
+    }
 });
 
 notes.delete('/:id', (req, res) => {
