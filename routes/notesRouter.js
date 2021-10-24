086282f18dc7c3e05f62c33c47d711d731b084b8
Ethan C <ethan.cho1826@gmail.com>
2021-10-24 15:43:45 -0500

    comments for better readablility of code

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,7 +1,7 @@
 const notes = require('express').Router();
 const {readFromFile, readAppend, writeToFile} = require('../helper/fsHelper');
 const ShortUniqueId = require('short-unique-id');
-
+// get route to display the data in the db.json
 notes.get('/', (req, res) => {
     console.log(`${req.method} received`);
     // res.status(200).json(notesData);
@@ -9,13 +9,14 @@ notes.get('/', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
-
+// post route to add data to db.json
 notes.post('/', (req, res) => {
     console.log(`${req.method} received`);
-    // console.log(req.body);
+    // destructures the body given from the fetch
     const {title, text} = req.body;
     if(title && text) {
         const uid = new ShortUniqueId({length:4});
+        // creates the object that's going to be stored in db.json
         const newNote = {title, text, id: uid()};
         console.log(newNote);
         readAppend(newNote, './db/db.json');
@@ -24,13 +25,13 @@ notes.post('/', (req, res) => {
             status: "success",
             body: newNote
         };
-
+        // gives the response object made above back
         res.json(response);
     }else {
         res.json("Error in posting note");
     }
 });
-
+// delete route deletes data from db.json
 notes.delete('/:id', (req, res) => {
     // TODO: readFile, filter array (note.id !== :id), return new array
     if(req.params.id) {
@@ -39,8 +40,10 @@ notes.delete('/:id', (req, res) => {
         const filteredDbArr = [];
         readFromFile('./db/db.json').then((data) => {
             dbArr = JSON.parse(data);
+            // takes filtered array takes in rest of the elements that doesn't match the id given in the parameter
             filteredDbArr.push(...dbArr.filter((obj) => obj.id != noteId));
             writeToFile('./db/db.json', filteredDbArr);
+            // gives the updated array stored back into db.json
             res.json(filteredDbArr);
         })
     }else {
--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const api = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use('/api', api);
-
+//get route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-
+// get route for the next page when user clicks on the button that sends them to the url
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
